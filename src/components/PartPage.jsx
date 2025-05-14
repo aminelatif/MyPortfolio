25d9eb49950878d3253cc810cee94e35a5027046
aminelatif <aminelatif53@gmail.com>
2025-05-14 04:50:27 +0100

    Remove 'Parties de cette leçon' section while keeping only Next Part button

--- src/components/PartPage.jsx
+++ src/components/PartPage.jsx
@@ -239,45 +239,19 @@ const PartPage = ({ part, allParts }) => {
 
         {/* Parts Navigation and Next Part Button */}
         <div className="mt-8 border-t pt-4">
-          <div className="flex flex-col space-y-4">
-            {/* Parts list */}
-            <div>
-              <h3 className="text-lg font-semibold mb-3">Parties de cette leçon</h3>
-              <div className="flex flex-wrap gap-2">
-                {parts.map((lessonPart, index) => (
-                  <button
-                    key={lessonPart.id}
-                    onClick={() => {
-                      const event = new CustomEvent('changePart', { detail: { partId: lessonPart.id } });
-                      document.dispatchEvent(event);
-                    }}
-                    className={`px-3 py-2 rounded text-sm ${
-                      lessonPart.id === part.id
-                        ? 'bg-blue-500 text-white'
-                        : 'bg-gray-100 hover:bg-gray-200 text-gray-700'
-                    }`}
-                  >
-                    {index + 1}. {lessonPart.title}
-                  </button>
-                ))}
-              </div>
+          {nextPart && (
+            <div className="flex justify-end">
+              <button
+                onClick={goToNextPart}
+                className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded flex items-center"
+              >
+                Partie suivante
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
+                </svg>
+              </button>
             </div>
-            
-            {/* Next Part button */}
-            {nextPart && (
-              <div className="flex justify-end">
-                <button
-                  onClick={goToNextPart}
-                  className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded flex items-center"
-                >
-                  Partie suivante
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
-                  </svg>
-                </button>
-              </div>
-            )}
-          </div>
+          )}
         </div>
       </div>
     </div>
